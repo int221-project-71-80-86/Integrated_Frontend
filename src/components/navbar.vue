32e7773699989ccc4d1b2288590b6061b6ae5019
surze <angpaono1@hotmail.com>
2021-04-28 10:00:53 +0700

    EDIT position of navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,6 +1,6 @@
 <template>
-    <nav id="header" class="w-full -ml-14 z-30 top-0 py-1">
-        <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3">
+    <nav id="header" class="w-full z-30 top-0 py-1 bg-white bg-opacity-90 sticky">
+        <div class="w-full container flex flex-wrap items-center justify-between mt-0 px-6 py-3">
             <label for="menu-toggle" class="cursor-pointer md:hidden block">
                 <svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
                     <title>menu</title>
@@ -9,22 +9,23 @@
             </label>
             <input class="hidden" type="checkbox" id="menu-toggle" />
 
-            <div class="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1" id="menu">
+            <div class="hidden md:flex md:items-center md:w-auto w-full" id="menu">
                 <nav>
                     <ul class="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
-                        <router-link to="../views/"><li><a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="#">HOME</a></li></router-link>
-                        <router-link to="../views/About"><li><a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="#">ABOUT</a></li></router-link>
+                        <router-link to="/"><li><a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="#">HOME</a></li></router-link>
+                        <router-link to="/About"><li><a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="#">ABOUT</a></li></router-link>
+                        <router-link to="/Products"><li><a class="inline-block no-underline hover:text-black hover:underline py-2 px-4" href="#">PRODUCTS</a></li></router-link>
                     </ul>
                 </nav>
             </div>
 
-            <div class="order-1 md:order-2">
-                <p class="flex items-center tracking-widest no-underline hover:no-underline font-bold text-gray-800 text-xl">
+            <div class="md:flex">
+                <p class="flex items-center tracking-widest no-underline hover:no-underline font-bold text-gray-800 text-xl mr-36">
                     SHOES-STORE
                 </p>
             </div>
 
-            <div class="order-2 md:order-3 flex items-center" id="nav-content">
+            <div class="md:flex flex items-center" id="nav-content">
 
                 <a class="pl-3 inline-block no-underline hover:text-black" href="#">
                     <router-link to="/cart"><svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
