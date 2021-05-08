b10bc07508e40517823a185bfbe45b659d179123
surze <angpaono1@hotmail.com>
2021-05-08 22:45:51 +0700

    edit duty

--- src/views/About.vue
+++ src/views/About.vue
@@ -19,17 +19,17 @@
       return{
       member: [{id:1,
                 fullname:'Paksawat Rakthai',
-                duty:'Front-End / Support',
+                duty:'Front-End / Dev-op (Network)',
                 studentcode:'62130500071',
                 img:'https://scontent.fkdt1-1.fna.fbcdn.net/v/t1.6435-9/90089361_2512863312361212_6000451341263044608_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=8bfeb9&_nc_eui2=AeHXjpE1FN56MhF2ynlFfsUjlvzjgFxCzbuW_OOAXELNu2ftAzO4Upffd4b85aPriDSMjToYHLcxzahljt2P1H55&_nc_ohc=LjD_UWB3bw0AX-153b4&_nc_ht=scontent.fkdt1-1.fna&oh=59bafb65daaf73fe58bb11880f9c4b2b&oe=60A4DA03'},
                 {id:2,
                 fullname:'Varot Tirakhuntorn',
-                duty:'Back-End / Support',
+                duty:'Back-End / Dev-op (ฺBackend) / Database',
                 studentcode:'62130500080',
                 img:'https://scontent.fkdt1-1.fna.fbcdn.net/v/t1.6435-9/67694126_2293753384007480_1202710230085926912_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=174925&_nc_eui2=AeHmYh1wIbfV4PeUYcT8bzlsWXzrTQIKTupZfOtNAgpO6klggnGyyCD0OxTLpAfHPZw0oJraLPgmjHR9fViIdvWM&_nc_ohc=_MBQCd1mNjMAX89gvOr&_nc_ht=scontent.fkdt1-1.fna&oh=7e28ee2a44c8ee43b05d2e55ff7bec0a&oe=60A6C5FC'},
                 {id:3,
                 fullname:'Wuttichai Arayasook',
-                duty:'Front-End / Support',
+                duty:'Front-End / Dev-ops (Frontend)',
                 studentcode:'62130500086',
                 img:'https://scontent.fkdt1-1.fna.fbcdn.net/v/t1.6435-9/125204886_2860048507562862_641813009569281055_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeEPd8oou2_O_op9UPTpBZGUFmbPqofn9PgWZs-qh-f0-IwtshLcQIR0buSksy7BYUwEmptZpXGcco0Uc4f_fycw&_nc_ohc=FrjzHM8MTOwAX9LVWBt&_nc_ht=scontent.fkdt1-1.fna&oh=6aef6173f8f3edc82bfc0b290e53afe8&oe=60A61FE5'}]
     }
